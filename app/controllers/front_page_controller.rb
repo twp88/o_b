8eb1a4b876a77fcfbcdce398db5e4895b9aa041c
Tom <tom.w.page@gmail.com>
2018-06-27 20:34:48 +0200

    Make FE changes

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -1,5 +1,18 @@
 class FrontPageController < ApplicationController
   def index
     @articles = Article.all
+    @about_me = about_me
+  end
+
+  protected
+
+  def about_me
+    "My name is Tom. I like to read and write, climb things, think about stuff
+    and drink coffee. From time to time I also like to code bits and pieces.
+    These are my cronicalled coding adventures. You might found some of this
+    stuff fairly inconsequential or uninteresting. That's because this is as
+    much for me, the writer, as it is for you the reader. As Martin Fowler says
+    'The best way that I can learn something is by writing an article about it.'
+    Enjoy!"
   end
 end
