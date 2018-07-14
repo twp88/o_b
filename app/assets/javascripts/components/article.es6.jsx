80b242b84e3ca0eab1ec81eb7047556db668eaa8
Tom <tom.w.page@gmail.com>
2018-07-14 19:29:39 +0200

    Add moments with utils

--- app/assets/javascripts/components/article.es6.jsx
+++ app/assets/javascripts/components/article.es6.jsx
@@ -5,10 +5,13 @@ class Article extends React.Component {
 
   render () {
     return (
-      <div className='article'>
+      <div>
         {this.props.articles.map(function(article){
           return(
-            <h2><a href={`/article/${article.id}`}>{article.title}</a></h2>
+            <div className='article'>
+              <h2><a href={`/article/${article.id}`}>{article.title}</a></h2>
+              <p>{formatTime(article.created_at)}</p>
+            </div>
           )}
          )
         }
